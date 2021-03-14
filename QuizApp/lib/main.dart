2918e9a68eba45f31557b627e444f9d729a47ba1
Tanim <42853370+tanim913@users.noreply.github.com>
2021-03-14 22:40:42 +0600

    Update main.dart

--- QuizApp/lib/main.dart
+++ QuizApp/lib/main.dart
@@ -7,9 +7,27 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+  void answerQuestion() {
+    questionIndex = questionIndex + 1;
+    print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var question = [
+      'What is your favorite color?',
+      'What\'s your favorite animal?'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -17,26 +35,34 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text('Question 1'),
+            Text(question[questionIndex]),
             RaisedButton(
               child: Text('Answer1'),
-              onPressed: null,
+              onPressed: () {
+                print("A");
+              },
             ),
             RaisedButton(
               child: Text('Answer2'),
-              onPressed: null,
+              onPressed: answerQuestion,
             ),
             RaisedButton(
               child: Text('Answer3'),
-              onPressed: null,
+              onPressed: () {
+                print("C");
+              },
             ),
             RaisedButton(
               child: Text('Answer4'),
-              onPressed: null,
+              onPressed: () {
+                print("D");
+              },
             ),
             RaisedButton(
               child: Text('Answer5'),
-              onPressed: null,
+              onPressed: () {
+                print("E");
+              },
             ),
           ],
         ),
