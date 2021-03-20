fe31420d351a3a088ccd8782d59c6201995e8e61
Tanim <42853370+tanim913@users.noreply.github.com>
2021-03-20 17:02:44 +0600

    Update result.dart

--- QuizApp/lib/result.dart
+++ QuizApp/lib/result.dart
@@ -1,8 +1,30 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int resultScore;
+  Result(this.resultScore);
+
+  String get resultPhase {
+    
+    var resultText;
+    if (resultScore <= 12) {
+      resultText = 'You are awesome and innocent\n' + 'Your Score is ' + resultScore.toString();
+    } else if (resultScore <= 20) { 
+      resultText = 'Pretty likable\n' + 'Your Score is ' + resultScore.toString();
+    } else if (resultScore <= 28) {
+      resultText = 'You are kinda Strange!\n' + 'Your Score is '+ resultScore.toString();
+    } else {
+      resultText = 'Seriously!! What is wrong with you?\n' + 'Your Score is '+ resultScore.toString();
+    } 
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Noice!'));
+    return Center(
+        child: Text(
+      resultPhase,
+      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+    ));
   }
 }
