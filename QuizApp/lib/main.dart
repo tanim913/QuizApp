35252be220a2ce98c0452a99eb87771eab9bf81b
Tanim <42853370+tanim913@users.noreply.github.com>
2021-03-20 17:02:56 +0600

    Update main.dart

--- QuizApp/lib/main.dart
+++ QuizApp/lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import './quiz.dart';
 import './result.dart';
@@ -19,36 +18,68 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  int _pscore;
   final _questions = const [
     {
       'questionText': 'What is your favorite color?',
-      'answers': ['Black', 'Red', 'White', 'Green']
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'White', 'score': 1},
+        {'text': 'Green', 'score': 3}
+      ]
     },
     {
       'questionText': 'What\'s your favorite animal?',
-      'answers': ['Cat', 'Dog', 'Human', 'Monkey']
+      'answers': [
+        {'text': 'Cat', 'score': 1},
+        {'text': 'Dog', 'score': 3},
+        {'text': 'Monkey', 'score': 5},
+        {'text': 'Human', 'score': 10}
+      ]
     },
     {
       'questionText': 'Who is your BJ Horseman Character?',
-      'answers': ['Todd', 'Mr. PB', 'PC', 'BJH']
+      'answers': [
+        {'text': 'Todd', 'score': 1},
+        {'text': 'Mr. PB', 'score': 5},
+        {'text': 'PC', 'score': 3},
+        {'text': 'BJH', 'score': 10}
+      ]
     },
     {
       'questionText': 'What is your favorite trait in a person?',
-      'answers': ['Kindness', 'Empathy', 'Genuinity', 'Transparency']
+      'answers': [
+        {'text': 'Transparency', 'score': 1},
+        {'text': 'Genuinity', 'score': 3},
+        {'text': 'Empathy', 'score': 5},
+        {'text': 'Kindness', 'score': 10}
+      ]
     },
     {
       'questionText': 'Which is your favorite web series?',
-      'answers': ['Breaking Bad', 'Dark', 'BoJack Horseman', 'Peaky Blinders']
+      'answers': [
+        {'text': 'Breaking Bad', 'score': 5},
+        {'text': 'Dark', 'score': 3},
+        {'text': 'BoJack Horseman', 'score': 1},
+        {'text': 'Peaky Blinders', 'score': 10}
+      ]
     }
   ];
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _answerQuestion(int score) {
     if (_questionIndex < _questions.length) {
       print('We have more questions');
     } else {
       print('No More questions');
     }
+    _pscore = score;
+    _totalScore += score;
+
+    print('total score is ' + _totalScore.toString());
+
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
@@ -59,6 +90,10 @@ class _MyAppState extends State<MyApp> {
     setState(() {
       _questionIndex = _questionIndex - 1;
     });
+    if (_questionIndex <= 0) {
+      _totalScore -= _pscore;
+    }
+    print('total score is ' + _totalScore.toString());
     print(_questionIndex);
   }
 
@@ -81,9 +116,9 @@ class _MyAppState extends State<MyApp> {
                 answerQuestion: _answerQuestion,
                 previousQuestion: _previousQuestion,
                 nextQuestion: _nextQuestion,
-                questions:_questions,
+                questions: _questions,
                 questionIndex: _questionIndex)
-            : Result(),
+            : Result(_totalScore),
       ),
     );
   }
